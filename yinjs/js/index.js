6981fcc1f1016677aabe731d2c01fb4fbb7704ae
pissalou <pascal.mazars@gmail.com>
2026-04-16 18:23:31 +0000

    Add initial implementation of audio processing with yin algorithm

---
+++ yinjs/js/index.js
@@ -0,0 +1,30 @@
+import yin from "./yin.mjs"
+
+function successCallback(stream) {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext
+    var audioContext = new AudioContext()
+
+    var analyser = audioContext.createAnalyser()
+    analyser.fftSize = Math.pow(2, 13)
+
+    var sampleRate = audioContext.sampleRate
+    var data = new Float32Array(analyser.fftSize)
+
+    function step() {
+        requestAnimationFrame(step)
+        analyser.getFloatTimeDomainData(data)
+        var frequency = yin(data, sampleRate)
+        console.log(frequency)
+    }
+
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream)
+    mediaStreamSource.connect(analyser)
+
+    requestAnimationFrame(step)
+}
+
+function errorCallback(err) {
+    alert(err)
+}
+
+navigator.getUserMedia({ audio: true }, successCallback, errorCallback)
